904fc573dff07a84eb32bdc7f5742a848844dc8c
tomer12901@gmail.com <tomer12901@gmail.com>
2020-08-13 13:26:58 +0300

    added README.md file

---
+++ README.md
@@ -0,0 +1,21 @@
+# EX1
+
+
+# Matrix
+The Matrix library contains commands executed on a matrix such as:
+Create a new matrix
+Copy a deep copy to another matrix
+Free up the memory we have allocated to a specific matrix
+Return the height of the matrix
+Return the width of the matrix
+Put a new value in a particular cell in the matrix
+Return the value of a particular cell in the matrix
+add matrices
+Multiply matrices
+Multiply matrix by scalar
+
+
+
+# ErrorCode
+The ErrorCode library contains commands that can check if there was an 
+error in one of the commands we made on the matrix and also return the error type
--- src/ErrorCode.c
+++ src/ErrorCode.c
@@ -8,19 +8,19 @@ bool error_isSuccess(ErrorCode code) {
 
 const char* error_getErrorMessage(ErrorCode code) {
 	switch (code) {
-	case ERROR_SUCCESS:
-		return "The operation completed successfully";
-	case SET_VALUE_ERROR:
-		return "ERROR: The column or row index given exceed the matrix boundaries or negative(in matrix_setValue function)!";
-	case MATRIX_ADD_ERROR:
-		return "ERROR: The matrix sizes are not equal and therefore it is not possible to perform an add operation(in matrix_add function)!";
-	case MATRIX_MULT_ERROR:
-		return "ERROR: The number of rows in the left matrix is not equal to the number of columns in the right matrix and therefore it is not possible to perform a multiplication operation(in matrix_multiplyMatrices function)!";
-	case NULL_ERROR:
-		return "ERROR: The matrix that given is null!";
-	case OUT_OF_MEMORY_ERROR:
-		return "ERROR: Allocation memory failed(in matrix_create function)!";
-	default:
-		return "Unexpected error";
+		case ERROR_SUCCESS:
+			return "The operation completed successfully";
+		case SET_VALUE_ERROR:
+			return "ERROR: The column or row index given exceed the matrix boundaries or negative(in matrix_setValue function)!";
+		case MATRIX_ADD_ERROR:
+			return "ERROR: The matrix sizes are not equal and therefore it is not possible to perform an add operation(in matrix_add function)!";
+		case MATRIX_MULT_ERROR:
+			return "ERROR: The number of rows in the left matrix is not equal to the number of columns in the right matrix and therefore it is not possible to perform a multiplication operation(in matrix_multiplyMatrices function)!";
+		case NULL_ERROR:
+			return "ERROR: The matrix that given is null!";
+		case OUT_OF_MEMORY_ERROR:
+			return "ERROR: Allocation memory failed(in matrix_create function)!";
+		default:
+			return "Unexpected error";
 	}
 }
